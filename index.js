e11fdae8732e9c29162832a73a97f071ff04a694
sourav kumar <souravkv0607@gmail.com>
2020-11-19 14:11:29 +0530

    temp change

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ require("./startup/db")();
 
 
 
-app.get("/",(req,res)=>{
-    res.send("website is live")
-    })
 const port = process.env.PORT || config.get("port");
 if(process.env.NODE_ENV==="production")
 {
     app.use(express.static('client/build'));
 }
 
+app.get("/",(req,res)=>{
+    res.send("website is live")
+    })
 const server=app.listen(port,()=>{
     console.log(`server listening at port: ${port}...`)
 })
